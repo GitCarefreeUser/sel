d97cec35a3452d76bb08564319319bd380b7cf4c
aydinserbest <aydinserbest34@gmail.com>
2023-01-06 00:56:11 +0100

    ilave YAPILDI

--- src/test/java/com/eurotech/excel/ExcelUtilDemo1.java
+++ src/test/java/com/eurotech/excel/ExcelUtilDemo1.java
@@ -3,18 +3,36 @@ package com.eurotech.excel;
 import org.testng.annotations.Test;
 import utilities.ExcelUtil;
 
+import java.util.Arrays;
 import java.util.List;
 import java.util.Map;
 
 public class ExcelUtilDemo1 {
     @Test
     public void readExcelFile() {
+
+        /*
+        ExcelUtil classinda excelden veriyi almak icin hazir metotlar var
+        o metotlar ile excelden istedigimiz veriyi cekicez
+        once o classin object'sini olustururuz
+        ExcelUtil classindaki constructor nedeniyle, object olustururken 2 String parametre gireriz
+        ilki:
+        ve parametre olarak buraya-framework'te resources altina yukledigimiz excelin
+        sag click copy path ile -path from content- diyerek -path'ini-aliriz
+        "" icine yapistiririz
+        ikinci parametremiz exceldeki sheet adimiz
+        onu da "" icine yazariz
+        !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+        ASLINDA AMAC bize hazir verilen exceldeki datayi kullanarak
+        o exceli @DataProvider anotatione'ina cevirmek
+         */
         ExcelUtil read=new ExcelUtil("src/test/resources/slm.xlsx","mrb");
 
         System.out.println(read.columnCount());
         System.out.println(read.rowCount());
         System.out.println(read.getColumnsNames());
 
+        //exceli map olarak alabiliriz:
         //map yapisinda key and value olarak alalim
         //column'daki baslik ve altindaki value seklinde
         //row row list'e aticak
@@ -48,6 +66,29 @@ public class ExcelUtilDemo1 {
 
          */
 
+        //[][] dimensional array'e de atabiliriz
+        String[][]dataArray=read.getDataArray();
+        //getDataArray() dedigimizde ilk row da geliyor
+        //ama excelde GENELDE ilk row'da basliklar olur
+        //ve gercek testte sadece value'lari kullanacagimiz icin
+        //bir baska metot kullaniriz
+        //getDataArrayWithoutFirstRow()
+        //burda simdilik bunu kullanmadik
+
+        System.out.println(Arrays.deepToString(dataArray));
+
+        /*
+        her bir row'u bir array olarak aticak
+        [country, city, vegetables, music],
+        [nederland, amsterdam, banana, classic],
+        ...gibi
+         */
+
+        /*
+        bu son metodu [] [] arraye atmakla @dataProvider annatotation'inindaki
+        hale getirmis olduk aslinda
+        ExcelUtilDemo2 classina BAKKKKKKKK!!!!!!!!
+         */
 
 
     }
