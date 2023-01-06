b80100e723b6de425cc55087e098b7f511ae36a9
aydinserbest <aydinserbest34@gmail.com>
2023-01-06 15:57:14 +0100

    ilave YAPILDI

---
+++ src/test/java/com/eurotech/tests/ExamplesTestBase/LoginPositiveTest.java
@@ -0,0 +1,16 @@
+package com.eurotech.tests.ExamplesTestBase;
+
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+
+public class LoginPositiveTest {
+
+    @FindBy(id="user-name")
+    public WebElement username;
+
+    @FindBy(css="input[name='password']")
+    public WebElement password;
+
+    @FindBy(xpath = "//input[@class='submit-button btn_action']")
+    public WebElement loginButton;
+}
