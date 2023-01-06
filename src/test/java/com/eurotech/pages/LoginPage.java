2d0d11c9505d315754171d7da695a88281fc7c1c
aydinserbest <aydinserbest34@gmail.com>
2023-01-07 00:19:19 +0100

    ilave YAPILDI

--- src/test/java/com/eurotech/pages/LoginPage.java
+++ src/test/java/com/eurotech/pages/LoginPage.java
@@ -1,7 +1,9 @@
 package com.eurotech.pages;
 
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindAll;
 import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.FindBys;
 
 public class LoginPage extends BasePage{
 
@@ -13,4 +15,28 @@ public class LoginPage extends BasePage{
 
     @FindBy(xpath = "//input[@class='submit-button btn_action']")
     public WebElement loginButton;
+
+    //usttekilere ilave bir de findall var
+    //ayni elementi iki ayri locate ederiz
+    //veya demek--ya biri ya biri hangisi varsa getir demek
+
+    @FindAll(
+            {
+                    @FindBy(id="login-button"),
+                    @FindBy(name = "login-button")
+            }
+            )
+    public WebElement loginButton2;
+
+    //ustteki findAll --> or-veya demekti
+    //her ikisini de sagla demek istedigimizde
+    @FindBys(
+            {
+                    @FindBy(id="login-button"),
+                    @FindBy(name = "login-button")
+            }
+    )
+            public WebElement loginButton3;
+
+
 }
