d7debe14ddf5578cc770bf6000cd9b801c36dfb8
aydinserbest <aydinserbest34@gmail.com>
2023-01-07 00:21:39 +0100

    ilave YAPILDI

--- src/test/java/com/eurotech/pages/LoginPage.java
+++ src/test/java/com/eurotech/pages/LoginPage.java
@@ -1,5 +1,6 @@
 package com.eurotech.pages;
 
+import com.eurotech.utilities.ConfigurationReader;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindAll;
 import org.openqa.selenium.support.FindBy;
@@ -7,15 +8,36 @@ import org.openqa.selenium.support.FindBys;
 
 public class LoginPage extends BasePage{
 
-    @FindBy(id="user-name")
-    public WebElement username;
+    @FindBy(name = "email")
+    public WebElement emailInput;
 
-    @FindBy(css="input[name='password']")
-    public WebElement password;
+    @FindBy(name = "password")
+    public WebElement passwordInput;
 
-    @FindBy(xpath = "//input[@class='submit-button btn_action']")
-    public WebElement loginButton;
+    @FindBy(xpath = "//input[@type='submit']")
+    public WebElement loginBtnLoginPage;
 
+    @FindBy(partialLinkText = "Forgot")
+    public WebElement forgotPassword;
+
+    @FindBy(xpath = "//*[text()='Invalid Credentials!']")
+    public WebElement invalidText;
+
+
+    public void loginTeacher(){
+        String email= ConfigurationReader.get("usernameTeacher");
+        String password =ConfigurationReader.get("passwordTeacher");
+        emailInput.sendKeys(email);
+        passwordInput.sendKeys(password);
+        loginBtnLoginPage.click();
+    }
+
+    public void login(String username,String password){
+        emailInput.sendKeys(username);
+        passwordInput.sendKeys(password);
+        loginBtnBasePage.click();
+    }
+    /*
     //usttekilere ilave bir de findall var
     //ayni elementi iki ayri locate ederiz
     //veya demek--ya biri ya biri hangisi varsa getir demek
@@ -39,4 +61,6 @@ public class LoginPage extends BasePage{
             public WebElement loginButton3;
 
 
+
+     */
 }
